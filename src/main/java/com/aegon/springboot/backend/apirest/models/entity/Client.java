93022ab42360eedfa79c4d6eed83767cec5299ce
Nicolás Chicaiza <jefryn@unicauca.edu.co>
2023-11-25 18:48:21 -0500

    :sparkles: (S4C37) Añadida la clase Entity Client al Backend

---
+++ src/main/java/com/aegon/springboot/backend/apirest/models/entity/Client.java
@@ -0,0 +1,76 @@
+package com.aegon.springboot.backend.apirest.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+/**
+ * Client
+ */
+@Entity
+@Table(name = "clients")
+public class Client implements Serializable {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  private String name;
+  private String lastname;
+  private String email;
+
+  @Column(name = "create_at")
+  @Temporal(TemporalType.DATE)
+  private Date createAt;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Date getCreateAt() {
+    return createAt;
+  }
+
+  public void setCreateAt(Date createAt) {
+    this.createAt = createAt;
+  }
+
+  private static final long serialVersionUID = 1L;
+
+}
