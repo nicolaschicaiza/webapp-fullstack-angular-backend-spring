08f40f069dc38cf36d917e91b8d0445ad05b1dbe
Nicolás Chicaiza <jefryn@unicauca.edu.co>
2023-11-26 09:17:34 -0500

    :sparkles: (S4C42) Uso de CORS para compartir recursos en API REST

--- src/main/java/com/aegon/springboot/backend/apirest/controllers/ClientRestController.java
+++ src/main/java/com/aegon/springboot/backend/apirest/controllers/ClientRestController.java
@@ -3,6 +3,7 @@ package com.aegon.springboot.backend.apirest.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.aegon.springboot.backend.apirest.models.services.IClientService;
 /**
  * ClientRestController
  */
+@CrossOrigin(origins = { "http://localhost:4200" })
 @RestController
 @RequestMapping("/api")
 public class ClientRestController {
