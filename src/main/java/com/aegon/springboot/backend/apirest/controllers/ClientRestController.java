8eb49b1d0e3f4feaca81419510ea8d1114096ddd
Nicolás Chicaiza <jefryn@unicauca.edu.co>
2023-12-05 20:54:48 -0500

    :sparkles: (S5C48) Prueba de Backend API Rest con Postman

--- src/main/java/com/aegon/springboot/backend/apirest/controllers/ClientRestController.java
+++ src/main/java/com/aegon/springboot/backend/apirest/controllers/ClientRestController.java
@@ -3,9 +3,16 @@ package com.aegon.springboot.backend.apirest.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aegon.springboot.backend.apirest.models.entity.Client;
@@ -34,13 +41,13 @@ public class ClientRestController {
   }
 
   @PostMapping("/clients")
-  @ResponseStatus(HttpStatus.CREATED)
+  @ResponseStatus(code = HttpStatus.CREATED)
   public Client create(@RequestBody Client client) {
     return clientService.save(client);
   }
 
   @PutMapping("/clients/{id}")
-  @ResponseStatus(HttpStatus.CREATED)
+  @ResponseStatus(code = HttpStatus.CREATED)
   public Client update(@RequestBody Client client, @PathVariable Long id) {
     Client currentClient = clientService.findById(id);
     currentClient.setName(client.getName());
